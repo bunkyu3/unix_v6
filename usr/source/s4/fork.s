36c5e1b7dfc678e21ea4e4c7312eaa6a0eb64ca9
bunkyu3 <pq9f07ebpr@gmail.com>
2019-05-15 05:30:56 +0900

    3.2 second

--- usr/source/s4/fork.s
+++ usr/source/s4/fork.s
@@ -3,4 +3,10 @@
 _fork:
 	mov		r5, -(sp)
 	mov		sp, r5
-	sys		fork
+	sys		fork		//forkシステムコール発行(sysがカーネルモードへの移行をしてるはず)
+
+	bec		2f			//システムコール中にエラーがなければ2へ
+
+2:
+	mov		(sp)+,r5
+	rts		pc			//_forkを呼び出した関数へリターン
--- usr/sys/ken/sys1.c
+++ usr/sys/ken/sys1.c
@@ -1,14 +1,26 @@
 fork(){
 	register struct proc *p1, *p2;
 
-	p1 = u.u_procp;									//現在実行中のプロセスをp1に
+	p1 = u.u_procp;									//現在実行中のプロセス(親)をp1に
 	for(p2 = & proc[0]; p2 < &proc[NPROC]; p2++)	//proc[]から空きエントリを検索
-		if(p2->p_stat == NULL)						// 見つかった
+		if(p2->p_stat == NULL)						// 見つかった, p2は子プロセス用
 			goto found;								// foundへ
 	u.u_error = EAGAIN;								//見つからなかったのでエラーコード格納
 	goto out;										//outへ
 
 found:
-	if(newproc()){
-	
+	if(newproc()){	//親のときは条件式は偽、子のときは真(switch関数のおかげ)となり{}内へ
+		u.u_ar0[R0] = p1->p_pid;	//子(ユーザプロセス)のr0レジスタに親のID
+		u.u_cstime[0] = 0;			//初期化?
+		u.u_sctime[1] = 0;			//初期化?
+		u.u_stime = 0;				//初期化?
+		u.u_cutime[0] = 0;			//初期化?
+		u.u_cutime[1] = 1;			//初期化?
+		u.u_utime = 0;				//初期化?
+		return;
 	}
+	u.u_ar0[R0] = p2->p_pid;	//親(ユーザプロセス)のr0レジスタに子のID
+
+out:
+	u.u_ar0[R7] =+ 2;			//親(ユーザプロセス)のPCを1命令文進める
+}
